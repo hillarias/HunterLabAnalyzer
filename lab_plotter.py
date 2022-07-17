84db0cea17317de8adabd43e248d81a080abcd87
hillarias <59289864+hillarias@users.noreply.github.com>
2022-07-17 08:57:13 -0700

    Update lab_plotter.py

--- lab_plotter.py
+++ lab_plotter.py
@@ -357,6 +357,7 @@ if ref_name is not None:
     de_table = de_table(table,l_star,a_star,b_star,ref_name)
     if de_table is not None:
         de_scatter = px.scatter(de_table, x = de_table.index , y = de_table.columns[-1])
+        de_scatter.update_layout( xaxis_title=None)
         st.header('ΔE2000 From Reference Point')
         st.write(de_table)
         st.subheader('ΔE2000 from ' + ref_name)
