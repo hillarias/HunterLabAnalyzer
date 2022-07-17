57dc6192da6b2016dd4f3a4af2fa24306eb0c9f8
hillarias <59289864+hillarias@users.noreply.github.com>
2022-07-16 23:39:47 -0700

    Update lab_plotter.py

--- lab_plotter.py
+++ lab_plotter.py
@@ -139,64 +139,64 @@ def lab_analyzer_std(uploaded_file):
         data = list(read_file)
     
     #removes unnecessary header from csv,always first 13 cells#
-    no_header = data[13:]
+        no_header = data[13:]
     
     
     #removeing all spectral data that is not lab values
-    no_sides = []
+        no_sides = []
 
-    for row in range(len(no_header)):
-        try:
-            if len(no_header[row][0]) == 0:
+        for row in range(len(no_header)):
+            try:
+                if len(no_header[row][0]) == 0:
+                    break
+                current_row = []
+                for col in range(5):
+                    current_row.append(no_header[row][col])
+                no_sides.append(current_row)
+
+            except IndexError:
                 break
-            current_row = []
-            for col in range(5):
-                current_row.append(no_header[row][col])
-            no_sides.append(current_row)
-        
-        except IndexError:
-            break
 
     
     
-    table = pd.DataFrame(no_sides,  dtype = float)
+        table = pd.DataFrame(no_sides,  dtype = float)
 
-    new_header = table.iloc[0] #grab the first row for the header
-    table = table[1:] #take the data less the header row
-    table.columns = new_header #set the header row as the df header
+        new_header = table.iloc[0] #grab the first row for the header
+        table = table[1:] #take the data less the header row
+        table.columns = new_header #set the header row as the df header
 
-    df = table.loc[: , ['Name','L*','a*','b*'] ] 
-    
-    
-    #removing _1,_2,_3..etc. from replicated experimental groups##
-    iterations = 10
-    for i in range(1, iterations +1):
-        df['Name'] = df['Name'].str.replace('_'+str(i),'')
-    
-    ##removing all underscores for appearance##
-    df['Name'] = df["Name"].str.replace('_',' ')
-    
-    ##setting index to experiment name and changing object type columns to float
-    df2 = df.set_index(['Name'])
-    df2["L*"] = pd.to_numeric(df2["L*"], downcast="float")
-    df2["a*"] = pd.to_numeric(df2["a*"], downcast="float")
-    df2["b*"] = pd.to_numeric(df2["b*"], downcast="float")
-    
-    ##grouping and rounding to 3 decimal places##
-    df_grouped = df2.groupby(['Name']).std()
-    l_star = np.round(list(df_grouped["L*"]) , 3)
-    a_star = np.round(list(df_grouped["a*"]) , 3)
-    b_star = np.round(list(df_grouped["b*"]) , 3)
-    
-    df_grouped["L*"] = l_star
-    df_grouped["a*"] = a_star
-    df_grouped["b*"] = b_star
-    
-    name_dictionary = {'L*': 'L*STD','a*':'a*STD', 'b*':'b*STD'}
-    
-    df_grouped = df_grouped.rename(columns= name_dictionary)
+        df = table.loc[: , ['Name','L*','a*','b*'] ] 
 
-    return df_grouped
+
+        #removing _1,_2,_3..etc. from replicated experimental groups##
+        iterations = 10
+        for i in range(1, iterations +1):
+            df['Name'] = df['Name'].str.replace('_'+str(i),'')
+
+        ##removing all underscores for appearance##
+        df['Name'] = df["Name"].str.replace('_',' ')
+
+        ##setting index to experiment name and changing object type columns to float
+        df2 = df.set_index(['Name'])
+        df2["L*"] = pd.to_numeric(df2["L*"], downcast="float")
+        df2["a*"] = pd.to_numeric(df2["a*"], downcast="float")
+        df2["b*"] = pd.to_numeric(df2["b*"], downcast="float")
+
+        ##grouping and rounding to 3 decimal places##
+        df_grouped = df2.groupby(['Name']).std()
+        l_star = np.round(list(df_grouped["L*"]) , 3)
+        a_star = np.round(list(df_grouped["a*"]) , 3)
+        b_star = np.round(list(df_grouped["b*"]) , 3)
+
+        df_grouped["L*"] = l_star
+        df_grouped["a*"] = a_star
+        df_grouped["b*"] = b_star
+
+        name_dictionary = {'L*': 'L*STD','a*':'a*STD', 'b*':'b*STD'}
+
+        df_grouped = df_grouped.rename(columns= name_dictionary)
+
+        return df_grouped
 
 def lab_analyzer_v3(filename):
     average_vals = lab_analyzer_mean(filename)
